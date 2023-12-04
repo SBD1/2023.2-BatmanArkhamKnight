3b0d784159913e97b05810053f13cdb4f51403cc
Luis Miranda <luiseduardocarneiro6@gmail.com>
2023-12-04 18:47:48 -0300

    Update Triggers&Procedures.sql

--- SQL/Triggers&Procedures.sql
+++ SQL/Triggers&Procedures.sql
@@ -8,20 +8,13 @@ CREATE OR REPLACE FUNCTION compra_equipamento(
 DECLARE
     valor_total INT;
 BEGIN
-    SELECT (CustoCompra * quantidade)
-    INTO custo_total
-    FROM Item
-    WHERE IdItem = item_id;
-
-    IF (SELECT Dinheiro FROM PC WHERE IdPlayer = jogador_id) >= custo_total THEN
-        INSERT INTO Inventario (PC, CapacidadeMaxima)
-        VALUES (jogador_id, 20)
-        ON CONFLICT (PC) DO NOTHING;
+    SELECT (equip_preco)
+    INTO equip_preco
+    FROM Equipamento
+    WHERE id = equipamento_id;
 
         INSERT INTO Equipamento (nome, descr, preco)
         VALUES (pc_id, equipamento_id, valor)
-        ON CONFLICT (Inventario, Item) DO UPDATE
-        SET Quantidade = InventarioItem.Quantidade + quantidade;
 
         RAISE NOTICE 'Compra realizada com sucesso!';
     ELSE
