b1d4be76f81ae9b17d4dcfee2abb455f9bb60519
Milena Baruc <102633138+MilenaBaruc@users.noreply.github.com>
2023-12-04 15:10:15 -0300

    add tabelas ddl

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -145,7 +145,7 @@ CREATE TABLE IF NOT EXISTS public.Veiculo(
     nome char(32) NOT NULL,
     CONSTRAINT id_PK PRIMARY KEY (id),
     CONSTRAINT localId_fk FOREIGN KEY (local_id)
-        REFERENCES public.local (id)
+        REFERENCES public.Local (id)
 );
 
 CREATE TABLE IF NOT EXISTS public.Personagem(
@@ -164,13 +164,13 @@ CREATE TABLE IF NOT EXISTS public.Habilidade(
 
 
 CREATE TABLE IF NOT EXISTS public.veic_possui_hab(
-    integer PRIMARY KEY(veic_id, hab_id),
-    integer FOREIGN KEY(veic_id, hab_id),
-    CONSTRAINT id_PK PRIMARY KEY (veic_id),
-    CONSTRAINT id_PK PRIMARY KEY (hab_id),
-    CONSTRAINT veic_FK FOREIGN KEY (veic_id)
+    veic_id integer,
+    hab_id integer,
+    CONSTRAINT veicId_PK PRIMARY KEY (veic_id),
+    CONSTRAINT habId_PK PRIMARY KEY (hab_id),
+    CONSTRAINT veicId_fk FOREIGN KEY (veic_id)
         REFERENCES public.Veiculo (id),
-    CONSTRAINT hab_FK FOREIGN KEY (hab_id)
+    CONSTRAINT habId_fk FOREIGN KEY (hab_id)
         REFERENCES public.Habilidade (id)
 );
 
@@ -178,12 +178,123 @@ CREATE TABLE IF NOT EXISTS public.Equipamento(
     id integer PRIMARY KEY,
     nome CHAR(32) NOT NULL,
     descr CHAR(128) NOT NULL,
-    alcance INTEGER NOT NULL,
+    preco INTEGER NOT NULL,
     CONSTRAINT id_PK PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS public.Efeito(
     id integer PRIMARY KEY,
-    efeito_nome CHAR(32) NOT NULL,
-    CONSTRAINT id_PK PRIMARY KEY (id),
+    nome INTEGER NOT NULL,
+    CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.pre_requisito(
+    missao_id INTEGER,
+    missao_pre_req_id INTEGER,
+    CONSTRAINT missaoId_PK PRIMARY KEY (missao_id),
+    CONSTRAINT missaoPreReqId_PK PRIMARY KEY (missao_pre_req_id),
+    CONSTRAINT missaoId_FK FOREIGN KEY (missao_id)
+        REFERENCES public.Missao (id),
+    CONSTRAINT missaoPreReqId_fk FOREIGN KEY (missao_pre_req_id)
+        REFERENCES public.Missao (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.cumpre_missao(
+    PC_id INTEGER,
+    missao_id INTEGER,
+    status INTEGER NOT NULL,
+    CONSTRAINT PCid_PK PRIMARY KEY (PC_id),
+    CONSTRAINT missaoId_PK PRIMARY KEY (missao_id),
+    CONSTRAINT PCid_FK FOREIGN KEY (PC_id)
+        REFERENCES public.PC (id),
+    CONSTRAINT missaoId_fk FOREIGN KEY (missao_id)
+        REFERENCES public.Missao (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.desbloqueia_equip(
+    missao_id INTEGER,
+    equip_id INTEGER,
+    CONSTRAINT equipId_PK PRIMARY KEY (equip_id),
+    CONSTRAINT missaoId_PK PRIMARY KEY (missao_id),
+    CONSTRAINT equipId_FK FOREIGN KEY (equip_id)
+        REFERENCES public.Equipamento (id),
+    CONSTRAINT missaoId_fk FOREIGN KEY (missao_id)
+        REFERENCES public.Missao (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.libera_missao(
+    missao_id INTEGER,
+    dial_id INTEGER,
+    CONSTRAINT missaoId_fk FOREIGN KEY (missao_id)
+        REFERENCES public.Missao (id),
+    CONSTRAINT dialId_FK FOREIGN KEY (dial_id)
+        REFERENCES public.Dialogo (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.guarda_dialogo(
+    PC_id INTEGER,
+    dial_id INTEGER,
+    CONSTRAINT PCid_PK PRIMARY KEY (PC_id),
+    CONSTRAINT dialId_PK PRIMARY KEY (dial_id),
+    CONSTRAINT PCid_FK FOREIGN KEY (PC_id)
+        REFERENCES public.PC (id),
+    CONSTRAINT missaoId_fk FOREIGN KEY (dial_id)
+        REFERENCES public.Dialogo (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.combate(
+    PC_id INTEGER,
+    NPC_id INTEGER,
+    distancia INTEGER NOT NULL,
+    CONSTRAINT PCid_PK PRIMARY KEY (PC_id),
+    CONSTRAINT NPCid_PK PRIMARY KEY (NPC_id),
+    CONSTRAINT PCid_FK FOREIGN KEY (PC_id)
+        REFERENCES public.PC (id),
+    CONSTRAINT NPCid_fk FOREIGN KEY (NPC_id)
+        REFERENCES public.NPC (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.PC_melhora_equip(
+    PC_id INTEGER,
+    equip_id INTEGER,
+    preco INTEGER NOT NULL,
+    CONSTRAINT PCid_PK PRIMARY KEY (PC_id),
+    CONSTRAINT equipId_PK PRIMARY KEY (equip_id),
+    CONSTRAINT PCid_FK FOREIGN KEY (PC_id)
+        REFERENCES public.PC (id),
+    CONSTRAINT equipId_FK FOREIGN KEY (equip_id)
+        REFERENCES public.Equipamento (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.pc_equipa(
+    PC_id INTEGER,
+    equip_id INTEGER,
+    CONSTRAINT PCid_PK PRIMARY KEY (PC_id),
+    CONSTRAINT equipId_PK PRIMARY KEY (equip_id),
+    CONSTRAINT PCid_FK FOREIGN KEY (PC_id)
+        REFERENCES public.PC (id),
+    CONSTRAINT equipId_FK FOREIGN KEY (equip_id)
+        REFERENCES public.Equipamento (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.pers_possui_hab(
+    person_id INTEGER,
+    hab_id INTEGER,
+    CONSTRAINT personId_PK PRIMARY KEY (person_id),
+    CONSTRAINT habId_PK PRIMARY KEY (hab_id),
+    CONSTRAINT personId_FK FOREIGN KEY (person_id)
+        REFERENCES public.Personagem (id),
+    CONSTRAINT habId_FK FOREIGN KEY (hab_id)
+        REFERENCES public.Habilidade (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.equip_tem_efeito(
+    equip_id INTEGER,
+    efeito_id INTEGER,
+    CONSTRAINT equipId_PK PRIMARY KEY (equip_id),
+    CONSTRAINT efeitoId_PK PRIMARY KEY (efeito_id),
+    CONSTRAINT equipId_FK FOREIGN KEY (equip_id)
+        REFERENCES public.Equipamento (id),
+    CONSTRAINT efeitoId_FK FOREIGN KEY (efeito_id)
+        REFERENCES public.Efeito (id)
 );
