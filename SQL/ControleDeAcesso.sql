e5179dbe24c6f598c71c25048f968e0743d39211
Milena Baruc <102633138+MilenaBaruc@users.noreply.github.com>
2023-12-04 17:14:54 -0300

    update

--- SQL/ControleDeAcesso.sql
+++ SQL/ControleDeAcesso.sql
@@ -7,7 +7,7 @@ create role owners;
 grant select, insert, update, delete on ALL TABLES IN SCHEMA public TO owners with grant option; -- Pode conceder permissão especificada a outros principais
 create user bat_jogo_owner with password 'bat_owner987' in role owners;
 
--- Role dos jogadores para acessa a view
+-- Role dos jogadores 
 create role jogadores;
 grant select, insert, delete on public.PC to jogadores with grant option;
 create user bat_jogadores with password 'bat_jogadores987' in role jogadores;
