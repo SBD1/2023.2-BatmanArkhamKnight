fcf80df1e2b1d1da6fc9b7fb39583f51e785c515
MateusPy <mateusorlando2014@gmail.com>
2023-12-07 20:21:24 -0300

    Adicionando criação de personagem no jogo

--- src/main.py
+++ src/main.py
@@ -2,16 +2,9 @@ import sys
 import os
 
 from typing import Optional
-from service.user_service import UserService
-from service.sala_service import SalaService
-from service.inventario_service import InventarioService
+from service.pc_service import PCService
 
-from repositories.sala_repository import SalaRepository
 
-from model.users import User
-from service.general_services import GeneralServices
-from repositories.inimigo_repository import InimigoRepository
-from model.inventario import Inventario
 
 def clear():
     # os.system('cls')
@@ -21,7 +14,7 @@ def clear():
 class Main:
 
     def __init__(self):
-        self.userService = UserService()
+        self.PCService = PCService()
         self.salaService = SalaService()
         self.salaRepository = SalaRepository()
         self.service = GeneralServices()
@@ -47,15 +40,13 @@ class Main:
             inp = input('> ')
 
             if inp == '1':
-                self.activeUser = self.userService.create()
-                self.activeInventary = self.inventarioService.getUserInventary(self.activeUser.id)
+                self.activePC = self.PCService.create()
                 self.play()
                 sys.exit()
                 
             if inp == '2':
-                self.activeUser = self.userService.login()
-                print(self.activeUser)
-                self.activeInventary = self.inventarioService.getUserInventary(self.activeUser.id)                
+                self.activePC = self.PCService.login()
+                print(self.activePC)               
                 
                 self.play()
                 sys.exit()
@@ -69,15 +60,14 @@ class Main:
 
 
     def play(self) -> Optional[bool]:
-        print(f'Login bem-sucedido no personagem {self.activeUser}')
+        print(f'Login bem-sucedido no personagem {self.activePC}')
         inp = 0
 
         while (inp not in [1, 2, 3]):
             # os.system('cls')
-            print('Escolha uma das opções abaixo(1-3):\n')
+            print('Escolha uma das opções abaixo(1/2):\n')
 
             print('1 - Mover de sala\n' +
-                '2 - Abrir inventario\n' +
                 '3 - Sair\n\n\n')
 
             print('Digite a opção desejada: \n')
@@ -85,21 +75,12 @@ class Main:
             inp = input('> ')
             
             if inp == '1':
-                print('inventario do player: ', self.activeInventary)
 
-                if self.activeUser != None and self.activeInventary != None:
-                    self.activeUser = self.salaService.mover(self.activeUser)
-                    self.checkSala(self.activeUser)
+                if self.activePC != None and self.activeInventary != None:
+                    self.activePC = self.salaService.mover(self.activePC)
+                    self.checkSala(self.activePC)
             
             elif inp == '2':
-                result = self.inventarioService.getInventaryWithItems(self.activeUser.id)
-                if result is not None:
-                    print(self.activeInventary)
-                    self.activeInventary = result
-                    print(self.activeInventary)
-
-            
-            elif inp == '3':
                 break
                 
             else:
@@ -109,7 +90,7 @@ class Main:
         if exit is not None:
             return False
     
-    def lutar(self, activeUser):
+    def lutar(self, activePC):
         print('Escolha uma das opções abaixo(1-2):\n')
 
         print('1 - Lutar\n' +
@@ -126,29 +107,29 @@ class Main:
                 return print("lutei")
             
             elif inp == '2':
-                self.activeUser = self.salaService.mover(self.activeUser)
-                self.checkSala(self.activeUser)    
+                self.activePC = self.salaService.mover(self.activePC)
+                self.checkSala(self.activePC)    
             
             else:
                 print('\nOpção Inválida!')
 
-    def checkSala(self, activeUser: User = None) -> Optional[User]:
+    def checkSala(self, activePC: PC = None) -> Optional[PC]:
         print(self.activeInventary)                
 
-        if self.inimigos.encontrarInimigos(activeUser) != None:
+        if self.inimigos.encontrarInimigos(activePC) != None:
             print('Você encontrou um inimigo!')
-            print(self.inimigos.encontrarInimigos(activeUser))
-            monstro = self.inimigos.encontrarInimigos(activeUser)
-            self.service.lutar(activeUser, monstro)
+            print(self.inimigos.encontrarInimigos(activePC))
+            monstro = self.inimigos.encontrarInimigos(activePC)
+            self.service.lutar(activePC, monstro)
         
-        if self.inimigos.encontrarBoss(activeUser) != None:
+        if self.inimigos.encontrarBoss(activePC) != None:
             print('Você encontrou um boss!')
-            monstro = self.inimigos.encontrarBoss(activeUser)
-            self.service.lutar(activeUser, monstro)
+            monstro = self.inimigos.encontrarBoss(activePC)
+            self.service.lutar(activePC, monstro)
         
-        if self.salaRepository.encontrarBau(activeUser) != None:
+        if self.salaRepository.encontrarBau(activePC) != None:
 
-            self.salaService.encontrarBau(activeUser, self.activeInventary.id)
+            self.salaService.encontrarBau(activePC, self.activeInventary.id)
             print('teste' + str(self.activeInventary))
 
 
