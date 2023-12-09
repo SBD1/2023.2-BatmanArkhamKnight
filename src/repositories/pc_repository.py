92a53f8327cf8b9be045483bb2aaab805dfabce5
Nick <nicolasbdbandeira@gmail.com>
2023-12-08 22:28:39 -0300

    .

--- src/repositories/pc_repository.py
+++ src/repositories/pc_repository.py
@@ -12,8 +12,8 @@ class PCRepository:
         with self.db.connection as conn:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    "INSERT INTO public.PC(NPC_alvo, HP, ATK, DEF, XP, furtividade, espaco, id_quadra, veic_id, descr) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                    [pc.NPC_alvo, pc.HP, pc.ATK, pc.DEF, pc.XP, pc.furtividade, pc.espaco, pc.id_quadra, pc.veic_id, pc.descr]
+                    "INSERT INTO public.PC(person_id, NPC_alvo, HP, ATK, DEF, XP, furtividade, espaco, id_quadra, veic_id, descr) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    [pc.person_id, pc.NPC_alvo, pc.HP, pc.ATK, pc.DEF, pc.XP, pc.furtividade, pc.espaco, pc.id_quadra, pc.veic_id, pc.descr]
                 )
 
     def updatePC(self, pc: PC) -> None:
@@ -21,8 +21,8 @@ class PCRepository:
         with self.db.connection as conn:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    "UPDATE public.PC SET NPC_alvo = %s, HP = %s, ATK = %s, DEF = %s, XP = %s, furtividade = %s, espaco = %s, id_quadra = %s, veic_id = %s, descr = %s WHERE person_id = %s",
-                    [pc.NPC_alvo, pc.HP, pc.ATK, pc.DEF, pc.XP, pc.furtividade, pc.espaco, pc.id_quadra, pc.veic_id, pc.descr, pc.person_id]
+                    "UPDATE public.PC SET person_id = %s, NPC_alvo = %s, HP = %s, ATK = %s, DEF = %s, XP = %s, furtividade = %s, espaco = %s, id_quadra = %s, veic_id = %s, descr = %s WHERE person_id = %s",
+                    [pc.person_id, pc.NPC_alvo, pc.HP, pc.ATK, pc.DEF, pc.XP, pc.furtividade, pc.espaco, pc.id_quadra, pc.veic_id, pc.descr, pc.person_id]
                 )
     
     def deletePC(self, person_id) -> None:
